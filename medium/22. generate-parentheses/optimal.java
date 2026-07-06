c494b2868159c899cabfdb4443d230c2d1fcf992
Ashritha <bl.sc.u4aie24251@bl.students.amrita.edu>
2026-07-06 11:40:58 +0530

    Implement generateParenthesis using backtracking

--- medium/22. generate-parentheses/optimal.java
+++ medium/22. generate-parentheses/optimal.java
@@ -1,1 +1,38 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        
+        List<String> result = new ArrayList<>();
 
+        backtrack(0, 0, n, new StringBuilder(), result);
+
+        return result;
+    }
+
+    private void backtrack(int open, int close, int n, StringBuilder current, List<String> result){
+        
+        if(current.length() == 2*n){
+
+            result.add(current.toString());
+            return;
+        }
+
+        if(open < n){
+
+            current.append('(');
+
+            backtrack(open+1, close, n, current, result);
+
+            current.deleteCharAt(current.length() - 1);
+        }
+
+        if(close < open){
+
+            current.append(')');
+
+            backtrack(open, close+1, n, current, result);
+
+            current.deleteCharAt(current.length() - 1);
+            
+        }
+    }
+}
