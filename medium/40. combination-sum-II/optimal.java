0d06ccd638981fe63d900a27c85ec3afdaecfd21
Ashritha <bl.sc.u4aie24251@bl.students.amrita.edu>
2026-07-09 23:27:01 +0530

    Implement combination sum II using backtracking

--- medium/40. combination-sum-II/optimal.java
+++ medium/40. combination-sum-II/optimal.java
@@ -1,1 +1,40 @@
+class Solution {
 
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+
+        Arrays.sort(candidates);
+        
+        backtrack(candidates, target, 0, new ArrayList<>());
+
+        return result;
+    }
+
+    private void backtrack(int[] candidates, int target, int start, List<Integer> current){
+
+        if(target == 0){
+
+            result.add(new ArrayList<>(current));
+                
+        }
+
+        for(int i = start; i < candidates.length; i++){
+
+            if(i > start && candidates[i] == candidates[i - 1]){
+                continue;
+            }
+
+            if(candidates[i] > target){
+                break;
+            }
+
+            current.add(candidates[i]);
+
+            backtrack(candidates, target - candidates[i], i+1, current);
+
+            current.remove(current.size() - 1);
+            
+        }
+    }
+}
