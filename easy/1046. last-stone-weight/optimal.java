22b43a3c7a4d9c3c798280dc905072bf5e2addf8
Ashritha <bl.sc.u4aie24251@bl.students.amrita.edu>
2026-07-14 20:04:54 +0530

    Create optimal.java

---
+++ easy/1046. last-stone-weight/optimal.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+
+        for(int nums : stones){
+
+            pq.offer(nums);
+        }
+
+        while(pq.size() > 1){
+
+            int x = pq.poll();
+            int y = pq.poll();
+
+            if(x != y){
+                pq.offer(Math.abs(x - y));
+            }
+        }
+
+        return pq.isEmpty() ? 0 : pq.peek();
+    }
+}
