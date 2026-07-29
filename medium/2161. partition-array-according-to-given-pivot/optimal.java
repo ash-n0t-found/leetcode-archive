7e94b23665f65045920891cd1475643375cf4539
Ashritha <bl.sc.u4aie24251@bl.students.amrita.edu>
2026-07-29 20:16:03 +0530

    Create optimal.java

---
+++ medium/2161. partition-array-according-to-given-pivot/optimal.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        
+        int[] res = new int[nums.length];
+        int index = 0;
+
+        for(int num : nums){
+
+            if(num < pivot) res[index++] = num; 
+        }
+
+        for(int num : nums){
+
+            if(num == pivot) res[index++] = num; 
+        }
+
+        for(int num : nums){
+
+            if(num > pivot) res[index++] = num; 
+        }
+
+        return res;
+    }
+}
